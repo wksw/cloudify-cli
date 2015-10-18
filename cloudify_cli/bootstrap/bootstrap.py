7f40f144275fda1d427981fa8b3d8b54854b5424
Noa Kuperberg <noakup@gmail.com>
2015-10-18 22:58:37 +0300

    hgf

--- cloudify_cli/bootstrap/bootstrap.py
+++ cloudify_cli/bootstrap/bootstrap.py
@@ -278,7 +278,11 @@ def _build_upload_provider_context_cmd(manager_node, update_context,
     security_enabled = security_config.get('enabled', False)
     protocol = constants.DEFAULT_REST_PORT
     verify_cert = ''
+    credentials_arg = ''
     if security_enabled:
+        username = security_config['admin_username']
+        password = security_config['admin_password']
+        credentials_arg = "-u '{0}':'{1}'".format(username, password)
         ssl_config = security_config.get('ssl', {})
         if ssl_config.get('enabled', False):
             rest_port = constants.SECURED_REST_PORT
@@ -288,10 +292,11 @@ def _build_upload_provider_context_cmd(manager_node, update_context,
 
     request_params = '?update={0}'.format(update_context)
     upload_provider_context_cmd = \
-        'curl --fail {0} -XPOST {1}://localhost:{2}/api/{3}/provider/context{4} -H ' \
-        '"Content-Type: application/json" -d @{5}'.format(
-            verify_cert, protocol, rest_port, constants.API_VERSION,
-            request_params, remote_provider_context_file)
+        'curl --fail {0} {1} -XPOST {1}://localhost:{3}/api/{4}/provider/context{5} -H ' \
+        '"Content-Type: application/json" -d @{6}'.format(
+            verify_cert, credentials_arg, protocol, rest_port,
+            constants.API_VERSION, request_params,
+            remote_provider_context_file)
     return upload_provider_context_cmd
 
 
